c8e324f6fbba09f2d2cdd0a46bbdb0ecb8e4697b
RoynBosses09 <roynajack303@gmail.com>
2024-08-31 17:37:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,36 +7,36 @@ global.audio = "";
 global.video = "https://telegra.ph/file/204481823350f368c1826.mp4";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "kkeizzah@gmail.com";
+global.email = "roynajack303@gmail.com";
 global.location = "Migori,Kenya";
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://wasitech10:WASImd##12@cluster0.eclhxiz.mongodb.net/?retryWrites=true&w=majority";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
-global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Nairobi";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Zimbabwe";
 global.github = process.env.GITHUB || "https://github.com/Keithkeizzah/KEITH-MD";
 global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47";
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vaan9TF9Bb62l8wpoD47";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/ad00848dda6847a67ffb0.jpg";
 global.devs = "254748387615";
-global.sudo = process.env.SUDO || "254748387615";
-global.owner = process.env.OWNER_NUMBER || "254748387615";
-global.style = process.env.STYLE || "5";
-global.gdbye = process.env.GOODBYE || "false";
-global.wlcm = process.env.WELCOME || "false";
-global.warncount = process.env.WARN_COUNT || 5;
+global.sudo = process.env.SUDO || "263774918784";
+global.owner = process.env.OWNER_NUMBER || "263774918784";
+global.style = process.env.STYLE || "1','2','3','4','5";
+global.gdbye = process.env.GOODBYE || "true";
+global.wlcm = process.env.WELCOME || "true";
+global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "true";
-global.disablegroup = process.env.DISABLE_GROUPS || "false",
+global.disablegroup = process.env.DISABLE_GROUPS || "true",
 global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
 global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/ad00848dda6847a67ffb0.jpg";
-global.waPresence = process.env.WAPRESENCE || "available";
+global.waPresence = process.env.WAPRESENCE || "available🤔😎👽";
 global.readcmds = process.env.READ_COMMAND || "true";
 global.readmessage = process.env.READ_MESSAGE || "false";
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "254748387615";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "263774918784";
 global.read_status = process.env.AUTO_READ_STATUS || "true";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
-global.read_status_from = process.env.READ_STATUS_FROM || "254748387615";
+global.read_status_from = process.env.READ_STATUS_FROM || "263774918784";
 
 global.api_smd = "https://api-smd.onrender.com";
 global.scan = "https://web.giftedtechnexus.co.ke";
@@ -46,14 +46,14 @@ global.SESSION_ID =
   ""
 module.exports = {
   menu: process.env.MENU || "2", /*Leave it in Default else bot will Crash*/
-  HANDLERS: process.env.PREFIX || ".",
+  HANDLERS: process.env.PREFIX || ".','#','$','+','&','@",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "3.0.0",
-  caption: process.env.CAPTION || "𝐌𝐈𝐆𝐇𝐓𝐘 𝐊𝐄𝐈𝐓𝐇-𝐌𝐃",
-  author: process.env.PACK_AUTHER || "Keith Tech",
-  packname: process.env.PACK_NAME || "Keith-Md♥️",
-  botname: process.env.BOT_NAME || "𝐊𝐄𝐈𝐓𝐇-𝐌𝐃",
-  ownername: process.env.OWNER_NAME || "𝐊𝐄𝐈𝐓𝐇𝐊𝐄𝐈𝐙𝐙𝐀𝐇",
+  caption: process.env.CAPTION || "𝐌𝐈𝐆𝐇𝐓𝐘 𝐊𝐄𝐈𝐓𝐇-𝐌𝐃😎👽",
+  author: process.env.PACK_AUTHER || "Keith Tech😎👽",
+  packname: process.env.PACK_NAME || "Keith-Md♥️😎👽",
+  botname: process.env.BOT_NAME || "𝐊𝐄𝐈𝐓𝐇-𝐌𝐃😎👽",
+  ownername: process.env.OWNER_NAME || "Mabhosiedu RoynJK😎👽",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "neLbXXrp8bSDcohnp1CW5UEa",
@@ -65,7 +65,7 @@ module.exports = {
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
-  LANG: (process.env.THEME || "KEITH").toUpperCase(),
+  LANG: (process.env.THEME || "KEITH😎👽").toUpperCase(),
 };
 global.rank = "updated";
 global.isMongodb = false;
